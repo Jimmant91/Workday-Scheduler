d90cdc75537a3b3e0d3e9fe54ba1dfd6d1404151
James Antley <jimmy.antley@gmail.com>
2021-04-30 00:19:08 -0400

    made grid container with combo of jquery and bootstrap

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
-var currentDay = moment().format("dddd LL");
+// page date and time
+var currentDay = moment().format("dddd LL LT");
 $("#currentDay").append(currentDay);
 
-var currentTime = moment().format("LT")
-$("#currentTime").append(currentTime)
+// possible grid container
+for (var i = 0; i < 9; i++) {
+    $('.container').append(
+        $(`<div id="row-0${i+1}"class="row time-block">
+
+            <div id="time-0${i+1}"
+            class="col-2 hour">
+            </div>
+
+            <textarea id="text-0${i+1}"
+            class="col-8">
+            </textarea>
+
+            <button class="col-2 saveBtn">Add</button>
+        
+        </div>`))
+}
