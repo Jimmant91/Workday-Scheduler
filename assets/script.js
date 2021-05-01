a65b0fa6eb10fcca5fefc003a9b1c843e9028caa
James Antley <jimmy.antley@gmail.com>
2021-05-01 15:50:21 -0400

    added all html bootstrap for grid

--- assets/script.js
+++ assets/script.js
@@ -2,47 +2,110 @@
 var currentDay = moment().format("dddd LL LT");
 $("#currentDay").append(currentDay);
 
-// possible grid container
-for (var i = 0; i < 9; i++) {
-    $('.container').append(
-        $(`<div id="row-0${i+1}"class="row time-block">
+///////////////////////////////////////////////////////////////
 
-            <div id="time-0${i+1}"
-            class="col-2 hour">
-            </div>
+// below is an example of how to create the grid through jquery
+// this option was less understandable to me
 
-            <textarea id="text-0${i+1}"
-            class="col-8">
-            </textarea>
+// for (var i = 0; i < 9; i++) {
+//     $('.container').append(
+//         $(`<div id="row-0${i+1}"class="row time-block">
 
-            <button class="col-2 saveBtn">Add</button>
+//             <div id="time-0${i+1}"
+//             class="col-2 hour">
+//             </div>
+
+//             <textarea id="text-0${i+1}"
+//             class="col-8">
+//             </textarea>
+
+//             <button class="col-2 saveBtn">Add</button>
         
-        </div>`))
+//         </div>`))
+// }
+///////////////////////////////////////////////////////////////
+
+
+// Empty array for times
+var timesList = [];
+
+// Variable for each hour element
+var hours = $(".hour");
+
+// Variable for current hour
+var currentHour = moment().format("HH");
+
+// Add hours from planner to a list
+// That way I have something to iterate over
+for (var i=0; i<hours.length; i++) {
+    var hourText = $(hours[i]).text();
+    timesList.push(hourText);
 }
 
-var saveBtn = $('.row').children('.saveBtn')
+// Variable for each time block element
+var timeBlock = $(".time-block");
 
-//adding hours to the time column
+// Empty array for textarea id's
+// Will use to assign class based on index
+var timeBlockList = [];
 
-for (var index = 8; index < 17; index++) {
-    if (index < 12) {
-        $('.row').children('.hour').eq(index - 8).text(`${index} AM`)
-    } else if (index === 12) {
-        $('.row').children('.hour').eq(index - 8).text(`${index} PM`)
-    } else {
-        $('.row').children('.hour').eq(index - 8).text(`${index - 12} PM`)
-    }
-    if (index == moment().format('H')) {
-        $('textarea').eq(index - 8).addClass('present')
-    } else if (index < moment().format('H')) {
-        $('textarea').eq(index - 8).addClass('past')
-    } else {
-        $('textarea').eq(index - 8).addClass('future')
-    }
-    $(`#text-0${index - 7}`).text(localStorage.getItem(`text-0${index - 7}`))
+// Add textarea id's to empty array for comparison later
+for (var i=0; i<timeBlock.length; i++) {
+    var timeBlockText = timeBlock[i].id;
+    timeBlockList.push(timeBlockText);
 }
-saveBtn.on('click', function () {
-    var textEl = $(this.parentNode).children('.col-md-10').attr('id');
-    var textToStorage = $(this.parentNode).children('.col-md-10').val();
-    localStorage.setItem(textEl, textToStorage)
-})
+
+
+// Now do a for loop to compare the array of times to the current time
+// Change the class of the given id based on the time condition
+for (var i=0; i<timesList.length; i++) {
+    var militaryTime = moment(timesList[i], "h A").format("HH");
+    if (militaryTime < currentHour) {
+        $("#" + timeBlockList[i]).addClass("past");
+    } else if (militaryTime === currentHour) {
+        $("#" + timeBlockList[i]).addClass("present");
+    } else {
+        $("#" + timeBlockList[i]).addClass("future");
+    }
+}
+
+
+// Okay, let's get to storing our data locally so it persists
+
+// Select the save buttons
+var saveBtn = $(".saveBtn");
+
+var inputStorageObject = {};
+
+// var inputText = $(this).parent().children("textarea").val();
+// var inputIdText = $(this).parent().children("textarea").attr("id");
+
+// Add event listener for save button
+// Store the text in a key:value pair >> key = id of textarea element  & value = text input
+saveBtn.on("click", function(event) {
+    event.preventDefault();
+    var inputText = $(this).parent().children("textarea").val();
+    var inputIdText = $(this).parent().children("textarea").attr("id");
+    inputStorageObject[inputIdText] = inputText;
+    localStorage.setItem("stored-text", JSON.stringify(inputStorageObject));
+});
+
+function init() {
+    var storedData = JSON.parse(localStorage.getItem("stored-text"));
+
+    if (storedData !== null) {
+        inputStorageObject = storedData;
+
+        var storedObject = Object.entries(inputStorageObject);
+
+        for (var i=0; i<storedObject.length; i++) {
+            var idInsert = storedObject[i][0];
+            var textInsert = storedObject[i][1];
+            $("#"+idInsert).text(textInsert);
+        }
+    }
+
+}
+
+
+init();
